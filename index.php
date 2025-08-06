5e9adfddc217c43fd029b2c896f8cec864c88e3b
Arthur Silva <arthurcom898estudos@gmail.com>
2025-08-06 15:49:47 -0300

    Using error conditional and adding json_decode function to return associative array

--- index.php
+++ index.php
@@ -7,5 +7,13 @@ $city = 'Osasco';
 $days = 3;
 
 $results = API::get($city, $days);
+
+if($results['status'] == 'error'){
+    echo $results['message'];
+    exit;
+}
+
+$data = json_decode($results['data'], true);
+
 echo '<pre>';
-print_r($results);
+print_r($data);
