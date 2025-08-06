5535884fdba6b5a31ead1638ff8be3eeac5bafd8
Arthur Silva <arthurcom898estudos@gmail.com>
2025-08-06 16:16:07 -0300

    Adding HTML Structure

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once 'inc/config.php';
 require_once 'inc/api.php';
 
 $city = 'Osasco';
+
 $days = 3;
 
 $results = API::get($city, $days);
@@ -41,3 +42,25 @@ foreach($data['forecast']['forecastday'] as $day) {
     $forescast_day['condition'] = $day['day']['condition']['text'];
     $forescast_day['condition_icon'] = $day['day']['condition']['icon'];      
 }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <title>API</title>
+</head>
+<body>
+    
+    <div class="container-fluid mt-5">
+        <div class="row justify-content-center mt-5">
+            <div class="col-10 p-5 bg-light text-black">
+                <h3>Tempo para a cidade <strong></strong></h3>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
