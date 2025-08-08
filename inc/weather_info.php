d87aace3bc3e0d67c135f1b673e32f6d6ad03b1b
Arthur Silva <arthurcom898estudos@gmail.com>
2025-08-08 16:07:36 -0300

    Declaring the icon image script from the  values returned by the api

--- inc/weather_info.php
+++ inc/weather_info.php
@@ -2,4 +2,11 @@
     <div>
         <?= $weather_info['info'] ?>
     </div>
+
+    <div class="d-flex align-itens-center">
+        <!-- icon -->
+        <div class="me-5">
+            <img src="<?= $weather_info['condition_icon'] ?>" class="img-fluid d-block">
+        </div>
+    </div>
 </div>
